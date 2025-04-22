d9c94f31530c5c80d4c2226bcd8904abf97d9875
Demid Shumakher <demid.shumakher@gmail.com>
2025-04-22 22:10:36 +0300

    add javadoc

--- src/main/java/ru/ifmo/se/utils/IOManagers/FileIOManager.java
+++ src/main/java/ru/ifmo/se/utils/IOManagers/FileIOManager.java
@@ -1,12 +1,15 @@
 package ru.ifmo.se.utils.IOManagers;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.Writer;
+import java.io.*;
 
-public class FileIOManager extends BaseIOManager{
-    public FileIOManager(String fileName, Writer output) throws FileNotFoundException {
+public class IOManagerWithoutOutput extends BaseIOManager{
+    public IOManagerWithoutOutput(String fileName, Writer output) throws FileNotFoundException {
         super(new BufferedReader(new FileReader(fileName)), output);
     }
+
+    @Override
+    public String readLine(String prompt) throws IOException {
+        return this.readLine("");
+    }
+
 }
